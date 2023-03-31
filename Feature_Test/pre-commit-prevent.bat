630f6548f3a6a481790484263acb7ccf57cd4e39
Vysh <vysh.simha@gmail.com>
2023-03-31 11:27:38 +0530

    done

--- Feature_Test/pre-commit-prevent.bat
+++ Feature_Test/pre-commit-prevent.bat
@@ -7,7 +7,7 @@ set "search_word=Red"
 for /r "%search_dir%" %%f in (*.csv) do (
   findstr /C:"%search_word%" "%%f" > nul
   if %errorlevel% equ 0 (
-    echo The word was found in the CSV file %%~nxf.
+    echo The word was found in the CSV file,no to commit %%~nxf.
   ) else (
     echo The word was not found in the CSV file %%~nxf.
   )
